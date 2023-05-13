de938a88b35404dabbbf868127cedfdaf62ae4b6
erikolvera <118490997+erikolvera@users.noreply.github.com>
2023-05-13 16:06:24 -0500

    a

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,12 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const util = require('util');
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     const notes =  JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
     res.json(notes);
 });
 
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
     const newNote = {
         title: req.body.title,
