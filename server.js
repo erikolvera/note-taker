305fc208407d0596d5b619ceef4f86c5bec3277b
erikolvera <118490997+erikolvera@users.noreply.github.com>
2023-05-13 16:07:36 -0500

    a

--- server.js
+++ server.js
@@ -9,5 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
